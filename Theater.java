5867870bf27e4d54e1959e45dade1ad81b45a426
sumashiva <sumareddy.xworkz@gmail.com>
2021-09-24 12:37:56 +0530

    first file upload

---
+++ Theater.java
@@ -0,0 +1,17 @@
+class Theater
+{
+String name;
+String location;
+int capacity=300;
+TicketPrice ticketPrice;
+
+
+Theater(String name,TicketPrice ticketPrice)
+{
+	this.name=name;
+	this.ticketPrice=ticketPrice;
+	
+	
+}
+
+}
