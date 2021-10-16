615d4f92efa2dafa3133fe238ddf2b3d13b748c2
sumashiva <sumareddy.xworkz@gmail.com>
2021-10-16 19:10:40 +0530

    access specifier

---
+++ PublicAccess.java
@@ -0,0 +1,18 @@
+package com.xworks.access;
+import com.xworkz.different_method.Public;
+
+public class PublicAccess {
+
+	public static void main(String[] accessing)
+	{
+		Public publicAccess=new Public();
+		
+		publicAccess.method1();
+		publicAccess.method2();
+		publicAccess.method3();
+		publicAccess.Public();
+		publicAccess.Public(23);
+		publicAccess.Public(sonu,24);
+		
+	}
+}
